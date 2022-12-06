d19e2b462166e3d5bc62d7c60a1f26f94d6f19af
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2022-12-06 18:14:52 +0500

    find02_max_count

--- find02_max_count.py
+++ find02_max_count.py
@@ -5,4 +5,11 @@ def find_max_count(data):
         data: list of numbers
     returns: count of maximum numbers in the list
     """
-    return 0
+    i=0
+    sum=data[0]
+    while i<len(data):
+        if sum<data[i]:
+            sum=data[i]
+        i+=1
+    return data.count(sum)
+print(find_max_count([4,9,5,2,5,7,9]))
