20fd852bb40dffc8ad11b7d874975be9b9266c37
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2022-12-07 00:54:41 +0500

    find10_min_even.py

--- find10_min_even.py
+++ find10_min_even.py
@@ -5,5 +5,18 @@ def find_min_even(data):
         data: list of numbers
     returns: minimum even number in the list
     """
-    return 0
+    i=0
+    even=[]
+    while i<len(data):
+        if data[i]%2==0:
+            even.append(data[i])
+        i+=1
+    j=0
+    max1=even[0]
+    while j<len(even):
+        if max1>even[j]:
+            max1=even[j]
+        j+=1
+    return max1
+print(find_min_even([1,7,9,90,789,6,4,32,12,1]))
 
