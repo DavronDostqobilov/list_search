cb4de6d153c63ca9d361a76c59c1842093abef6a
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2022-12-07 00:49:38 +0500

    find08_min_count

--- find08_min_count.py
+++ find08_min_count.py
@@ -5,4 +5,11 @@ def find_min_count(data):
         data: list of numbers
     returns: count of minimum numbers in the list
     """
-    return 0
+    i=0
+    mn=data[0]
+    while i<len(data):
+        if mn>data[i]:
+            mn=data[i]
+        i+=1
+    return data.count(mn)
+print(find_min_count([1,3,4,6,8,-32,-2,-32]))
