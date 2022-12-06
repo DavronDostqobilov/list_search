54d192a2d303dc14fe9fb47607fba155c8532427
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2022-12-07 00:51:29 +0500

    find09_min_index.py

--- find09_min_index.py
+++ find09_min_index.py
@@ -5,5 +5,12 @@ def find_min_index(data):
         data: list of numbers
     returns: index of minimum number in the list
     """
-    return 0
+    i=0
+    mn=data[0]
+    while i<len(data):
+        if mn>data[i]:
+            mn=data[i]
+        i+=1
+    return data.index(mn)
+print(find_min_index([1,3,4,6,8,-32,-2]))
 
